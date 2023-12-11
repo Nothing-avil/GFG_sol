f800e281df89e7e2571a8aa0973205e06a35d834
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2023-12-11 19:01:21 +0530

    Create Max_Sum_Subarray_of_size_K.cpp

---
+++ Max_Sum_Subarray_of_size_K.cpp
@@ -0,0 +1,16 @@
+class Solution{   
+public:
+    long maximumSumSubarray(int k, vector<int> &arr , int N){
+        long maxs=0, res=0;
+        for(int i=0;i<k; i++){
+            maxs+= arr[i];
+        }
+        res= max(res, maxs);
+        for(int i=k;i<N; i++){
+            maxs-=arr[i-k];
+            maxs+=arr[i];
+            res=max(res, maxs);
+        }
+        return res;
+    }
+};
