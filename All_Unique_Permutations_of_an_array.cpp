4f28a3a4aac276bee20eb8e81425645d9ee83d94
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-01-17 23:56:05 +0530

    Create All_Unique_Permutations_of_an_array.cpp

---
+++ All_Unique_Permutations_of_an_array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    vector<vector<int>> uniquePerms(vector<int> &arr ,int n) {
+        vector<vector<int>>store;
+        sort(arr.begin(),arr.end());
+        do{
+            store.push_back(arr);
+        }while(next_permutation(arr.begin(),arr.end()));
+        
+        
+        return store;
+    }
+};
