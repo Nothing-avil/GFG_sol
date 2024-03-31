710ab32662c6e970b429528b6aacd4e5049c3369
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-03-31 23:36:51 +0530

    Create Closest_Neighbour_in_BST.cpp

---
+++ Closest_Neighbour_in_BST.cpp
@@ -0,0 +1,24 @@
+//Structure of the Node of the BST is as
+struct Node {
+    int key;
+    Node* left, *right;
+};
+
+
+   'N' is the element
+   'size' is the total element in BST
+  */
+
+class Solution {
+  public:
+  
+    int findMaxForN(Node* root, int n) {
+        if(root==NULL){
+            return -1;
+        }
+        if(root->key==n){
+            return n;
+        }
+        return root->key<n? (max(root->key, findMaxForN(root->right, n))):( min(root->key, findMaxForN(root->left, n)));
+    }
+};
