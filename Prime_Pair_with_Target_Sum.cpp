d177c85f6dc1a7ac28f519e68e635f6aba3ed3f7
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-06-16 23:09:51 +0530

    Create Prime_Pair_with_Target_Sum.cpp

---
+++ Prime_Pair_with_Target_Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<int> getPrimes(int n) {
+        bool prime[n+1];
+        memset(prime,true,sizeof prime);
+        prime[0]=false;
+        prime[1]=false;
+        for(int i=2;i*i<=n;i++){
+            if(prime[i]){
+                for(int j=i*i;j<=n;j+=i)prime[j]=false;
+            }
+        }
+        for(int i=2;i<=n;i++){
+            if(prime[i] and prime[n-i])return {i,n-i};
+        }
+        return {-1,-1};
+    }
+};
