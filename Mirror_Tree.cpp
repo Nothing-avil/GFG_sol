de03e45f188ae3c1fcab5b3b8282732fdf90d4fb
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-09-13 22:59:51 +0530

    Create Mirror_Tree.cpp

---
+++ Mirror_Tree.cpp
@@ -0,0 +1,28 @@
+// function Template for C++
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child /
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* node) {
+        if(node ==NULL){
+            return;
+        }
+        mirror(node->left);
+        mirror(node->right);
+        swap(node->left, node->right);
+    }
+};
