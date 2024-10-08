b4c63ceaea8a5a85d2bb32f18fe49775b330edd5
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2024-10-08 19:11:30 +0530

    Create Largest_Pair_Sum.cpp

---
+++ Largest_Pair_Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int pairsum(vector<int> &arr) {
+        int n = arr.size();
+        int largest = -1, secondLargest = -1;
+        for (int i = 0; i < n; i++) {
+            if(arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] < largest && arr[i] > secondLargest) {
+                secondLargest = arr[i];
+            }
+        }
+        return (largest+secondLargest);
+    }
+};
