21733ac3c99b02d549ada8de8595e4c320f68743
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-01-19 20:49:50 +0530

    Create Rotate_a_Linked_List.cpp

--- Rotate_a_Linked_List.cpp
+++ Rotate_a_Linked_List.cpp
@@ -1,18 +1,19 @@
 class Solution {
   public:
-    // Function to rotate a linked list.
     Node* rotate(Node* head, int k) {
-        // Your code here
-        Node* p= head;
-        while(p->next != NULL){
-            p = p->next;
+        Node* last = head;
+        int s =1;
+        while(last->next != NULL){
+            last = last->next;
+            s++;
         }
-        for(int i=1; i<=k; i++){
-            Node* t = head;
+        k = k%s;
+        while(k-- > 0){
+            Node* f = head;
             head = head->next;
-            t->next = NULL;
-            p->next = t;
-            p = t;
+            f->next = NULL;
+            last->next = f;
+            last = f;
         }
         return head;
     }
