295caa121f6b3091f41988195365bd9b55826d17
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-02-25 23:51:32 +0530

    Create Histogram_Max_Rectangular_Area.cpp

---
+++ Histogram_Max_Rectangular_Area.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int getMaxArea(vector<int> &heights) {
+        int res = 0, n = heights.size();
+        stack<int> stk;
+        vector<int> left(n, -1);
+        vector<int> right(n, n);
+        for (int i = 0; i < n; ++i) {
+            while (!stk.empty() && heights[stk.top()] >= heights[i]) {
+                right[stk.top()] = i;
+                stk.pop();
+            }
+            if (!stk.empty()) left[i] = stk.top();
+            stk.push(i);
+        }
+        for (int i = 0; i < n; ++i)
+            res = max(res, heights[i] * (right[i] - left[i] - 1));
+
+        return res;
+    }
+};
