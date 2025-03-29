3c6efb032cdbf754ca664bca51f0da00996d8da9
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-03-29 23:42:21 +0530

    Update Job_Sequencing_Problem.cpp

--- Job_Sequencing_Problem.cpp
+++ Job_Sequencing_Problem.cpp
@@ -29,3 +29,43 @@ class Solution
         return {day,profit};
     } 
 };
+
+//Another approach
+
+class Solution {
+  public:
+    vector<int> jobSequencing(vector<int> &deadline, vector<int> &profit) {
+        int n = deadline.size();
+        vector<pair<int, int>> jobs(n);
+
+        int maxDays = 0;
+        for (int i = 0; i < n; i++) {
+            jobs[i] = {profit[i], deadline[i]};
+            maxDays = max(maxDays, deadline[i]);
+        }
+
+        sort(jobs.rbegin(), jobs.rend());
+
+        vector<int> parent(maxDays + 1);
+        for (int i = 0; i <= maxDays; i++) parent[i] = i;
+        function<int(int)> find = [&](int day) {
+            if (parent[day] == day) return day;
+            return parent[day] = find(parent[day]);
+        };
+
+        int jobCount = 0, maxProfit = 0;
+
+        for (auto& job : jobs) {
+            int p = job.first, d = job.second;
+
+            int availableDay = find(d); // Find closest available day
+            if (availableDay > 0) {
+                parent[availableDay] = find(availableDay - 1); // Update parent (union)
+                jobCount++;
+                maxProfit += p;
+            }
+        }
+
+        return {jobCount, maxProfit};
+    }
+};
