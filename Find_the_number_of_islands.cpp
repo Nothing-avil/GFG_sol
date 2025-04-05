e2fa6b66e5a8288d3fb02fa6696918e99996a9d0
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-04-05 23:24:35 +0530

    Update Find_the_number_of_islands.cpp

--- Find_the_number_of_islands.cpp
+++ Find_the_number_of_islands.cpp
@@ -1,12 +1,11 @@
 class Solution {
   public:
-    // Function to find the number of islands.
     void dfs(vector<vector<char>>& grid, int i, int j, int row, int col){
         
-        if(i<0 || i>=row || j<0 || j>=col || grid[i][j] != '1')
+        if(i<0 || i>=row || j<0 || j>=col || grid[i][j] != 'L')
             return;
         
-        grid[i][j]='0';
+        grid[i][j]='W';
 
         dfs(grid, i+1,j, row, col);
         dfs(grid, i+1,j+1, row, col);
@@ -18,12 +17,12 @@ class Solution {
         dfs(grid, i+1,j-1, row, col);
     }
     
-    int numIslands(vector<vector<char>>& grid) {
+    int countIslands(vector<vector<char>>& grid) {
         int c=0;
         int row=grid.size(), col=grid[0].size();
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
-                if(grid[i][j] == '1'){
+                if(grid[i][j] == 'L'){
                     c++;
                     dfs(grid, i, j, row, col);
                 }
