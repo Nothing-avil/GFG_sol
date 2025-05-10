4b55fe0063790c214f75c36fb5944bf1cc4eb79d
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-05-10 23:03:24 +0530

    Create Longest_Subarray_with_Majority_Greater_than_K.cpp

---
+++ Longest_Subarray_with_Majority_Greater_than_K.cpp
@@ -0,0 +1,26 @@
+// User function Template for C++
+class Solution {
+  public:
+    int longestSubarray(vector<int> &arr, int k) {
+        int answer = 0, temp = 0;
+        unordered_map<int, int> mp;
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i] <= k){
+                temp--;
+            }
+            else{
+                temp++;   
+            }
+            if(temp <= 0 && mp.count(temp)){
+                mp[answer] = i;
+            }
+            if(temp <= 0 && mp.count(temp - 1)){
+                answer = max(answer, i - mp[temp - 1]);
+            }
+            if(temp > 0){
+                answer = i+1;
+            }
+        }
+        return answer;
+    }
+};
