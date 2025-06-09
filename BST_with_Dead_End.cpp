6854aaba786241273944b130b6138f3367e4d77c
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-06-09 23:33:21 +0530

    Create BST_with_Dead_End.cpp

---
+++ BST_with_Dead_End.cpp
@@ -0,0 +1,48 @@
+/*The Node structure is
+class Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};*/
+
+class Solution{
+  public:
+      vector<int> arr, leaf;
+    
+    void inorder(Node* node){
+        if(!node)
+            return;
+        
+        inorder(node->left);
+        arr.push_back(node->data);
+        if(!node->left && !node->right)
+            leaf.push_back(node->data);
+        inorder(node->right);
+    }
+    
+    bool isDeadEnd(Node *root)
+    {
+        arr = {0}; 
+        leaf = {};
+        inorder(root);
+        
+        int j = 0;
+        for(int i = 1; i < arr.size() && j < leaf.size(); ++i){
+            if(arr[i] == leaf[j]){
+                if(arr[i-1] == leaf[j] - 1 && arr[i+1] == leaf[j] + 1)
+                    return true;
+                ++j;
+            }
+        }
+        
+        return false;
+    }
+};
+
