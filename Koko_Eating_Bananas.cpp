d1c36d38dae89dc7fd683471edc5308d7ec52504
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-06-13 23:37:26 +0530

    Create Koko_Eating_Bananas.cpp

---
+++ Koko_Eating_Bananas.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int kokoEat(vector<int>& arr, int k) {
+        int n = arr.size();
+        int left = 1, right = *max_element(arr.begin(), arr.end());
+        int result = right;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int totalHours = 0;
+            for (int i = 0; i < n; i++) {
+                totalHours += (arr[i] + mid - 1)
+            }
+            if (totalHours <= k) {
+                result = mid;          // possible answer, but try to minimize
+                right = mid - 1;
+            } else {
+                left = mid + 1;        // too slow, need to eat faster
+            }
+        }
+
+        return result;
+    }
+};
