08a7df86f7da01423292748fe1ba94fe2a9d2675
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-08 23:54:02 +0530

    Create Next_element_with_greater_frequency.cpp

---
+++ Next_element_with_greater_frequency.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    vector<int> findGreater(vector<int>& arr) {
+        int n = arr.size();
+      
+         map<int , int >mp;
+         stack<int>st;
+         for(it :arr){
+             mp[it]++;
+         }
+         vector<int>ans(n,-1);
+         for(int i=0 ; i<n ; i++){
+             while(!st.empty() && mp[arr[i]]>mp[arr[st.top()]]){
+                 ans[st.top()]= arr[i];
+                 st.pop();
+             }
+             st.push(i);
+         }
+     return ans ;
+    }
+};
