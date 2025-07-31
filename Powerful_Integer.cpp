80dc5480348c0c567e3303c3ca9a40b4a3fc8a7e
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-07-31 22:01:29 +0530

    Create Powerful_Integer.cpp

---
+++ Powerful_Integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int powerfulInteger(vector<vector<int>>& intervals, int k) {
+        unordered_map<int, pair<int, int>> mp;
+        for(auto it: intervals){
+            mp[it[0]].first++;
+            mp[it[1]].second--;
+        }
+        int answer = -1, c = 0;
+        for(auto &i : mp){
+            c += i.second.first;
+            if(c >= k){
+                answer = i.first;
+            }
+            c += i.second.second;
+        }
+        return answer;
+    }
+};
