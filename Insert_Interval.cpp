bfd6ca7dea882253a6f141378a6c6512507e1f92
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-08-15 22:56:08 +0530

    Create Insert_Interval.cpp

--- Insert_Interval.cpp
+++ Insert_Interval.cpp
@@ -2,19 +2,18 @@ class Solution {
   public:
     vector<vector<int>> insertInterval(vector<vector<int>> &intervals, vector<int> &newInterval) {
         intervals.push_back(newInterval);
-        sort(intervals.begin(),intervals.end());
-        vector<vector<int>> v;
-        for(const auto& i:intervals)
-        {
-            if(v.empty() || v.back()[1]<i[0])
-            {
-                v.push_back(i);
+        vector<vector<int>> answer;
+        sort(intervals.begin(), intervals.end());
+        for(auto it : intervals){
+            if(!answer.size() || it[0] > answer.back()[1]){
+                answer.push_back(it);
+                continue;
             }
-            else if(v.back()[1]>=i[0] && v.back()[1]<i[1])
-            {
-                v.back()[1]=i[1];
-            }
+            auto row = answer.back();
+            answer.pop_back();
+            row[1] = max(it[1], row[1]);
+            answer.push_back(row);
         }
-        return v;
+        return answer;
     }
 };
