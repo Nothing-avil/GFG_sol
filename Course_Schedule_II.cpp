a3b6e84f032c20ab25bc522004f758df7094b529
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-11-01 23:03:41 +0530

    Create Course_Schedule_II.cpp

---
+++ Course_Schedule_II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    vector<int> findOrder(int n, vector<vector<int>> &prerequisites) {
+        vector<vector<int>> g(n);
+        vector<int> ind(n,0);
+        queue<int> q;
+        vector<int> ans;
+        for(auto x:prerequisites){
+            g[x[1]].push_back(x[0]);
+            ind[x[0]]++;
+        }
+        for(int i=0; i<n; i++){
+            if(ind[i] == 0) q.push(i);
+        }
+        while(!q.empty()){
+            int f = q.front();
+            q.pop();
+            ans.push_back(f);
+            for(auto x:g[f]){
+                ind[x]--;
+                if(ind[x] == 0) q.push(x);
+            }
+        }
+        if(ans.size()!=n) return {};
+        return ans;
+    }
+};
