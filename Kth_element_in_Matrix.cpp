98967ba9405a169466c9f421bffe6dcfd9620b27
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2025-12-27 23:14:54 +0530

    Create Kth_element_in_Matrix.cpp

--- Kth_element_in_Matrix.cpp
+++ Kth_element_in_Matrix.cpp
@@ -2,17 +2,14 @@ class Solution {
   public:
     int kthSmallest(vector<vector<int>> &matrix, int k) {
         // code here
-        int m=matrix.size();
+        int m=mat.size();
         int n=m*m;
         int arr[n];
         int a=0;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<m;j++)
-        {
-            arr[a++]=matrix[i][j];
-        }
-            
+        for(int i=0;i<m;i++){
+            for(int j=0;j<m;j++){
+                arr[a++]=mat[i][j];
+            }
         }
         sort(arr,arr+a);
         return arr[k-1];
