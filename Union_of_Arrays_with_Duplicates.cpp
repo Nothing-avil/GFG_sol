c1a661765d9aad6da2264829807551aa5c40e177
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-02-23 22:58:43 +0530

    Update Union_of_Arrays_with_Duplicates.cpp

--- Union_of_Arrays_with_Duplicates.cpp
+++ Union_of_Arrays_with_Duplicates.cpp
@@ -10,5 +10,25 @@ class Solution {
             mp[i]++;
         }
         return mp.size();
+
+      /*
+  class Solution {
+  public:
+    vector<int> findUnion(vector<int>& a, vector<int>& b) {
+        unordered_map<int, int> mp;
+        for(int i : a){
+            mp[i]++;
+        }
+        for(int i : b){
+            mp[i]++;
+        }
+        vector<int> ans;
+        for(auto i : mp){
+            ans.push_back(i.first);
+        }
+        return ans;  //if we have to return the elements
+    }
+  };
+      */
     }
 };
