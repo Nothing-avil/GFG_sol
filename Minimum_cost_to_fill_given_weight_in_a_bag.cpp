b8fb1bc8b3a65e8520ab639136b4f3e23075036d
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-06-15 23:25:43 +0530

    Create Minimum_cost_to_fill_given_weight_in_a_bag.cpp

--- Minimum_cost_to_fill_given_weight_in_a_bag.cpp
+++ Minimum_cost_to_fill_given_weight_in_a_bag.cpp
@@ -1,13 +1,22 @@
 class Solution {
-  public:
-    int minimumCost(int n, int w, vector<int> &cost) {
-        vector<int> dp(w+1, INT_MAX);
-        dp[0]=0;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<w+1; j++){
-                dp[j]= min(dp[j], cost[i]+dp[j-i-1]);
-            }
-        }
-        return dp[w];
-    }
+	public:
+	int minimumCost(vector<int> &cost, int w) {
+		int n = cost.size();
+		vector<int> dp(w + 1, INT_MAX);
+		for (int i = 0; i<w + 1; i++)
+			dp[i] = (int)1e5 + 1;
+		
+		dp[0] = 0;
+		for (int i = 0; i<n; i++) {
+			if (cost[i] == -1) {
+				continue;
+			}
+			int wt = i + 1;
+			for (int cur = wt; cur <= w; cur++) {
+				if (dp[cur - wt] != (int)1e5 + 1)
+					dp[cur] = min(dp[cur], dp[cur - wt]+cost[i]);
+			}
+		}
+		return (dp[w] == (int)1e5 + 1)?-1:dp[w];
+	}
 };
