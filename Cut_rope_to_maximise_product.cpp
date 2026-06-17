12630502b3ff06ca49ca8263afae8c7b5af48b67
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-06-17 23:20:07 +0530

    Create Cut_rope_to_maximise_product.cpp\

---
+++ Cut_rope_to_maximise_product.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int maxProduct(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        int ans = 1;
+        while (n > 4) {
+            ans *= 3;
+            n -= 3;
+        }
+        return ans * n;
+    }
+};
