1cab50c5af3d9a39d21ebe11dc334671137e4f56
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2026-06-18 23:57:52 +0530

    Update Coverage_of_all_Zeros_in_a_Binary_Matrix.cpp

--- Coverage_of_all_Zeros_in_a_Binary_Matrix.cpp
+++ Coverage_of_all_Zeros_in_a_Binary_Matrix.cpp
@@ -1,27 +1,29 @@
 class Solution {
   public:
-    int findCoverage(vector<vector<int>>& matrix) {
-        int n=matrix.size(),m=matrix[0].size(),count=0;
-        vector<int> row={-1,1,0,0};
-        vector<int> col={0,0,-1,1};
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    for(int k=0;k<4;k++)
-                    {
-                        int rc=i+row[k];
-                        int cc=j+col[k];
-                        if(rc>=0 && rc<n && cc>=0 && cc<m && matrix[rc][cc]==1)
-                        count++;
-                    }
+    int findCoverage(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size(), total = 0;
+        vector<bool> up(n, false), left(m, false),
+        right(m, false), down(n, false);
+        for(int i=0; i<mat.size(); i++) {
+            for(int j=0; j<mat[i].size(); j++) {
+                if(mat[i][j] == 0) {
+                    if(left[i]) total++;
+                    if(up[j]) total++;
+                }
+                if(mat[i][j] == 1) {
+                    left[i] = true;
+                    up[j]   = true;
+                }
+                if(mat[m-i-1][n-j-1] == 0) {
+                    if(right[m-i-1]) total++;
+                    if(down[n-j-1]) total++;
+                }
+                if(mat[m-i-1][n-j-1] == 1) {
+                    right[m-i-1] = true;
+                    down[n-j-1] = true;
                 }
             }
         }
-        return count;
-
+        return total;
     }
 };
